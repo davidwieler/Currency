c12877831423dbb5f3dcfc364396b9b12e0070f2
David Wieler <davidw@evanshunt.com>
2017-02-12 15:31:08 -0700

    Adjust routes

--- routes.js
+++ routes.js
@@ -1,15 +1,8 @@
 module.exports = function(app, passport, express) {
 
-    app.use(function(req, res, next) {
-
-        next();
-
-    });
-
     //Routing files
     var api = require('./routes/api.js');
     var front = require('./routes/front.js');
-
 
     //Routing functions
     //
@@ -18,13 +11,11 @@ module.exports = function(app, passport, express) {
 
     // Handle 404
     app.use(function(req, res, next) {
-        //spry.track('Server Logs', {'error': {httpstatuscode: '404'}, 'page': req.url})
         res.status(404).send('<h1>ERROR 404:</h1>Page not found! OOPS!');
     });
 
     // Handle 500
     app.use(function(error, req, res, next) {
-        //spry.track('Server Logs', {'error': {httpstatuscode: '500', stackTrace: error.stack}})
         res.status(500).send('<h1>ERROR 500:</h1>Error loading services<br />'+error+'<br />'+error.stack);
     });
 
